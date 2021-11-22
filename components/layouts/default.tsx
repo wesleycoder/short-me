ce8787c775da3e3a2b0f55047e88a0abc65ce23a
Wésley Queiroz <wesley.coder@gmail.com>
2021-11-22 09:56:00 -0300

    Basic styles

--- components/layouts/default.tsx
+++ components/layouts/default.tsx
@@ -1,9 +1,10 @@
 import { PropsWithChildren } from "react";
+import styles from "./default.module.css";
 
 export default function Layout({ children }: PropsWithChildren<{}>) {
   return (
-    <>
+    <div className={styles.container}>
       {children}
-    </>
+    </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,15 +4,26 @@ import styles from "./Home.module.css";
 
 const Home: NextPage = () => {
   return (
-    <>
+    <div className={styles.container}>
       <Head>
         <title>Short me</title>
         <meta name="description" content="Link shortener" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Hello</h1>
-    </>
+      <h1 className={styles.title}>Short me</h1>
+      <form className={styles.form}>
+        <label htmlFor="link" className={styles.label}>
+          Paste your link below and we&#39;ll shorten it for you:
+        </label>
+        <div className={styles.formGroup}>
+          <input name="link" type="text" className={styles.input} />
+          <button type="submit" className={styles.button}>
+            Shorten
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
 
