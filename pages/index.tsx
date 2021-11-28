d10fc8969ce6aa621a028977c5611e907cd619fe
Wésley Queiroz <wesley.coder@gmail.com>
2021-11-28 16:03:07 -0300

    feat(Home): call api on submit
    Render placeholder with current host url
    Add typescript types for lodash and valid-url

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,42 @@
-import type { NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
 import Head from "next/head";
+import { useCallback, useEffect, useState } from "react";
+import { TextInput } from "../components/atoms";
+import { getRequestHostUrl, validateUrl } from "../utils";
 import styles from "./Home.module.css";
 
-const Home: NextPage = () => {
+interface HomeProps {
+  host?: string;
+}
+
+const useHostURL = (defaultHost: string = "https://short.me") => {
+  const [host, setHost] = useState(defaultHost);
+
+  useEffect(() => {
+    setHost(window?.location?.href || defaultHost);
+  }, [defaultHost]);
+
+  return host;
+};
+
+const Home: NextPage<HomeProps> = ({ host }) => {
+  const placeholder = useHostURL(host);
+  const [url, setUrl] = useState("");
+  const [isValid, setIsValid] = useState(true);
+
+  const onSubmit = useCallback(async (e) => {
+    e.preventDefault();
+    if (validateUrl(url)) {
+      await fetch("/api/shorten", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ url }),
+      });
+    }
+  }, [url]);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,13 +46,30 @@ const Home: NextPage = () => {
       </Head>
 
       <h1 className={styles.title}>Short me</h1>
-      <form className={styles.form}>
-        <label htmlFor="link" className={styles.label}>
+      <form
+        action="/api/shorten"
+        method="POST"
+        noValidate
+        onSubmit={onSubmit}
+        className={styles.form}
+      >
+        <label htmlFor="url" className={styles.label}>
           Paste your link below and we&#39;ll shorten it for you:
         </label>
         <div className={styles.formGroup}>
-          <input name="link" type="text" className={styles.input} />
-          <button type="submit" className={styles.button}>
+          <TextInput
+            name="url"
+            type="url"
+            defaultValue={url}
+            className={[styles.input, !isValid && styles.invalid].filter(
+              Boolean,
+            ).join(" ")}
+            onChangeText={setUrl}
+            onValidate={setIsValid}
+            validate={validateUrl}
+            placeholder={placeholder}
+          />
+          <button disabled={!isValid} type="submit" className={styles.button}>
             Shorten
           </button>
         </div>
@@ -26,5 +77,11 @@ const Home: NextPage = () => {
     </div>
   );
 };
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => ({
+  props: {
+    host: `${getRequestHostUrl(ctx.req).origin}/`,
+  },
+});
 
 export default Home;
