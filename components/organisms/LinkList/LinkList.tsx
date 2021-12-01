173460bec60d93498f6084af57c4c5da04160a31
Wésley Queiroz <wesley.coder@gmail.com>
2021-12-01 13:34:19 -0300

    feat: LinkList organism

---
+++ components/organisms/LinkList/LinkList.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import { ComponentProps } from "react";
+import { HashedUrl } from "../../../db";
+import { useHostURL } from "../../../utils/hooks/useHostUrl";
+import styles from "./LinkList.module.css";
+
+interface Props extends ComponentProps<"ul"> {
+  links: HashedUrl[];
+}
+
+export const LinkList = ({ links = [], ...props }: Props) => {
+  const host = useHostURL();
+  return (
+    <ul {...props} className={styles.linkList}>
+      {links.map(({ url, hash }) => (
+        <li key={hash}>
+          <Link href={hash}>{hash}</Link>
+          <br />
+          {url}
+        </li>
+      ))}
+    </ul>
+  );
+};
